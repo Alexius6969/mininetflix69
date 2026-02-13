1eb6a9be3e610fc4a848925bcfd3eb180d7d1562
Alexius6969 <aleale180400@gmail.com>
2026-02-13 15:23:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,45 +8,24 @@ const users = {
   "Chiara": "iii"
 };
 
+// ... (INSERISCI QUI TUTTI I TUOI DATI seriesData, animeData, moviesData) ... 
+// Per brevità non li ricopio tutti, usa quelli che hai già nel tuo script con le stagioni 1, 2, 3
+
 const seriesData = {
   "Ginny e Georgia": {
     img: "https://i.imgur.com/CScKmEZ.jpeg",
     seasons: {
       1: [
         { episode: 1, title: "Pilot", src: "https://drive.google.com/file/d/12Ew2SflLxJP6C6UuznxfX4ou-1e_1Xcq/preview" },
-        { episode: 2, title: "E' un viso, non una maschera", src: "https://drive.google.com/file/d/13wB1kyWtuHqINWpBYsfRSgbOIQ8404UE/preview" },
-        { episode: 3, title: "Cose che si addicono solo a gente ricca", src: "https://drive.google.com/file/d/1SagpLPK4OmTmeO0dr23xfeA1rfkmFFv4/preview" },
-        { episode: 4, title: "Lydia Bennet è senza dubbio femminista", src: "https://drive.google.com/file/d/1uM6S1cUnBRT_wRLsxvX0VO5oZ6MAIbVU/preview" },
-        { episode: 5, title: "Boo, brutta stron*a", src: "https://drive.google.com/file/d/1MFYR1es-2PzE40642-nuaMJQ7GE0QHOZ/preview" },
-        { episode: 6, title: "Non mi capite", src: "https://drive.google.com/file/d/1Rfr4vh6xVEoG5f-WMeeQyoChIGInwQAt/preview" },
-        { episode: 7, title: "Buon compleanno sciocca", src: "https://drive.google.com/file/d/1Kq7Fs06knX-6VOXQTs0hqphJ3Q3srCun/preview" },
-        { episode: 8, title: "Croce su una parte, croce sull'altra", src: "https://drive.google.com/file/d/1kLJ29kNZnz_RF5NDSXVJvSdWUHe6TC5J/preview" },
-        { episode: 9, title: "Amare è difficile", src: "https://drive.google.com/file/d/1-1Au2tWQIj37QW_PFj3mG71i_k4YS3Rc/preview" },
-        { episode: 10, title: "Il peggior tradimento dai tempi di Jordyn e Kylie", src: "https://drive.google.com/file/d/1jv_pet7AY2Yvrwa0-HmZc_GRGcEeu01o/preview" }
+        // ... (Tuoi episodi Stagione 1)
       ],
       2: [
         { episode: 1, title: "Bentornati str**zetti", src: "https://drive.google.com/file/d/13hz6WDbRZN0iN000HEjvPNVrhhO1f75p/preview" },
-        { episode: 2, title: "Perchè tutto deve diventare cosi terribile, ogni volta, sempre?", src: "https://drive.google.com/file/d/1rstuICrKjXthkzgbm_dIYXPeM-updvTK/preview" },
-        { episode: 3, title: "Che cosa hai in mente, ragazzina?", src: "https://drive.google.com/file/d/1OHwtpnbItdGgEzPnc-eT7xDjD5__2c_2/preview" },
-        { episode: 4, title: "Buon mio compleanno a voi", src: "https://drive.google.com/file/d/1FJFa-iI6Q0uSc7IFPKI9R-a_xjaWdK5w/preview" },
-        { episode: 5, title: "I latkes sono eccellenti", src: "https://drive.google.com/file/d/1CXzJxprhWFgAfYFsVRxmwjcEM76coGhY/preview" },
-        { episode: 6, title: "Questo è lo speciale Natale di Ginny & Georgia", src: "https://drive.google.com/file/d/1iWx2hEi-H7BcVC4W-S84mxED2kk-D6NY/preview" },
-        { episode: 7, title: "Lascia fare a noi tutto il resto", src: "https://drive.google.com/file/d/1YISd0u7tw9DY38-RQdji_6Mt_SNTBQqQ/preview" },
-        { episode: 8, title: "Ascolta! L'oscurità scende...", src: "https://drive.google.com/file/d/19_nawVSd7rWaVffpUJSPd_97XAgGYtIp/preview" },
-        { episode: 9, title: "Prima o poi lo uccido", src: "https://drive.google.com/file/d/1SYAA10m4LjxaAItsbypN_eK7K9i7IyGl/preview" },
-        { episode: 10, title: "Non sono Cenerentola", src: "https://drive.google.com/file/d/1z6ea1hdBPUG3NqaEVv3TE5-YlrAU_lA-/preview" }
+        // ... (Tuoi episodi Stagione 2)
       ],
       3: [
         { episode: 1, title: "Non sarebbe un buon podcast", src: "https://drive.google.com/file/d/1UuRAg2JStiRt-X2Q63NH6vN3NADVcInX/preview" },
-        { episode: 2, title: "Maledetto bip, bip", src: "https://drive.google.com/file/d/10ipOz6ugcE4hI9JtHyQZJu1azsLuFHgk/preview" },
-        { episode: 3, title: "Gli amici ballano", src: "https://drive.google.com/file/d/1pjbdrEG9mpSxP1vvtSlKoqzbPLW7fb6n/preview" },
-        { episode: 4, title: "E' tornata la str***a", src: "https://drive.google.com/file/d/18LgMYOeJoMJ9N101H1DOkKnxwQzurj3p/preview" },
-        { episode: 5, title: "La dinamite fa bum", src: "https://drive.google.com/file/d/1lYcyzTPVuqTdC5yDG2E_gQyXOOFeHNvX/preview" },
-        { episode: 6, title: "Almeno non può andare peggio", src: "https://drive.google.com/file/d/1F-_xjgRZy4SLFgGB9EOOuUfrImfcXmoS/preview" },
-        { episode: 7, title: "E' pazzesco", src: "https://drive.google.com/file/d/19T_zEO-PXZZl8uK3Wsu3eDfqQMF8qn5v/preview" },
-        { episode: 8, title: "E' un pranzo al sacco?", src: "https://drive.google.com/file/d/1CxoJWegDyFbvtu2XsRZTE5tnVr3geM1x/preview" },
-        { episode: 9, title: "E' il momento del mio assolo", src: "https://drive.google.com/file/d/1ZUnk12DxDTH3E36rx7sLH1Qyctwu-Rfv/preview" },
-        { episode: 10, title: "Mostri", src: "https://drive.google.com/file/d/1UvEuk_dt2Fv2uwlB_X5NoQ6su4VavjB0/preview" }
+        // ... (Tuoi episodi Stagione 3)
       ]
     }
   },
@@ -78,7 +57,6 @@ const moviesData = {
 
 let currentUser = null;
 let currentSerieTitle = ""; 
-let currentSelectedSeason = null;
 
 // --- LOGIN ---
 function login() {
@@ -185,7 +163,7 @@ function loadHome() {
   filterCategory('home');
 }
 
-// --- SEASON SELECTOR (STILE NETFLIX) ---
+// --- FUNZIONE SHOW SEASONS (CON MENU A TENDINA) ---
 function showSeasons(title, type) {
   currentSerieTitle = title;
   hideAllViews();
@@ -198,56 +176,53 @@ function showSeasons(title, type) {
       document.querySelector('.details-hero').style.backgroundImage = `linear-gradient(to top, #141414, transparent), url('${dataObj.img}')`;
   }
 
-  // Popoliamo il selettore di stagioni
-  const seasonSelector = document.getElementById("season-selector");
-  seasonSelector.innerHTML = "";
+  const container = document.querySelector("#season-list .container-padding");
+  container.innerHTML = ""; 
+
+  // 1. MENU A TENDINA
+  const selectorContainer = document.createElement("div");
+  selectorContainer.className = "season-selector-container";
+  const select = document.createElement("select");
+  select.className = "season-select";
   
-  if (dataObj.seasons) {
-    const seasonKeys = Object.keys(dataObj.seasons);
-    seasonKeys.forEach(s => {
-      const option = document.createElement("option");
-      option.value = s;
-      option.textContent = `Stagione ${s}`;
-      seasonSelector.appendChild(option);
-    });
-    
-    // Mostriamo la prima stagione di default
-    currentSelectedSeason = seasonKeys[0];
-    displayEpisodesForSeason(title, currentSelectedSeason, type);
+  const seasonKeys = Object.keys(dataObj.seasons); 
+  seasonKeys.forEach(sKey => {
+    const option = document.createElement("option");
+    option.value = sKey;
+    option.textContent = "Stagione " + sKey;
+    select.appendChild(option);
+  });
+
+  selectorContainer.appendChild(select);
+  container.appendChild(selectorContainer);
+
+  // 2. LISTA EPISODI
+  const ul = document.createElement("ul");
+  ul.id = "seasons-ul";
+  container.appendChild(ul);
+
+  function renderEpisodes(seasonNum) {
+    ul.innerHTML = "";
+    if (dataObj.seasons[seasonNum]) {
+      dataObj.seasons[seasonNum].forEach(ep => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <div style="display:flex; flex-direction:column;">
+                <span style="font-weight:bold; font-size:1rem;">${ep.episode}. ${ep.title}</span>
+            </div>
+            <span>▶</span>
+        `;
+        li.onclick = () => playVideo(ep.src, ep.title);
+        ul.appendChild(li);
+      });
+    }
   }
+
+  select.onchange = function() { renderEpisodes(this.value); };
+
+  if (seasonKeys.length > 0) { renderEpisodes(seasonKeys[0]); }
 }
 
-function changeSeason() {
-  const seasonSelector = document.getElementById("season-selector");
-  const selectedSeason = seasonSelector.value;
-  currentSelectedSeason = selectedSeason;
-  
-  // Determina il tipo (serie o anime)
-  let type = 'serie';
-  if (animeData[currentSerieTitle]) {
-    type = 'anime';
-  }
-  
-  displayEpisodesForSeason(currentSerieTitle, selectedSeason, type);
-}
-
-function displayEpisodesForSeason(title, seasonNum, type) {
-  let dataObj = (type === 'anime') ? animeData[title] : seriesData[title];
-  
-  const ul = document.getElementById("seasons-ul");
-  ul.innerHTML = "";
-
-  if (dataObj.seasons && dataObj.seasons[seasonNum]) {
-    dataObj.seasons[seasonNum].forEach(ep => {
-      const li = document.createElement("li");
-      li.innerHTML = `<span>${ep.episode}. ${ep.title}</span> <span>▶</span>`;
-      li.onclick = () => playVideo(ep.src, ep.title);
-      ul.appendChild(li);
-    });
-  }
-}
-
-// --- PLAYER E NAVIGAZIONE INTERNA ---
 function playVideo(src, title) {
   hideAllViews();
   document.getElementById("episode-list").style.display = "block";
