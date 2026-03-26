8f3979cfc4ba47d3b8a838ea3bbe411adade089b
Alexius6969 <aleale180400@gmail.com>
2026-03-26 13:17:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const users = {
 const seriesData = {
   "Ginny e Georgia": {
     img: "https://i.imgur.com/CScKmEZ.jpeg",
-    trailer: "Z2R2aD8A9X0", 
+    trailer: "9uBrr25Gwj4", 
     desc: "Una madre dallo spirito libero e i suoi figli si trasferiscono al nord per ricominciare da capo in questa serie drammatica e commovente.",
     seasons: {
       1: [
